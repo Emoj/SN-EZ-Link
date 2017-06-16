9cfbc06095aa3b8f6e3845fda5fd7e25f1726d23
= <sirawitbm@gmail.com>
2017-06-16 12:30:41 +0800

    modify for readability

--- try.py
+++ try.py
@@ -6,8 +6,8 @@ from networkx.algorithms import bipartite
 # [i for i, v in enumerate(locationList) if v[0] == 1 and v[1] == 1]
 
 
-def to_seconds(s):
-    hh, mm, ss = [int(x) for x in s.split(':')]
+def to_seconds(line):
+    hh, mm, ss = [int(x) for x in line.split(':')]
     return hh*3600 + mm*60 + ss
 
 
@@ -39,14 +39,14 @@ B.add_nodes_from(locationList, bipartite=1)
 
 for t in df.itertuples():
     agent, location, time = t[1], (t[2], t[3]), (t[4], t[5])
-    B.add_edge(agent, location, t = time)
+    B.add_edge(agent, location, t=time)
 
 agentSet = set(n for n,d in B.nodes(data=True) if d['bipartite']==0)
 locationSet = set(B) - agentSet
-    
-closeness_centrality = bipartite.closeness_centrality(B, locationSet)
-degree_centrality = bipartite.degree_centrality(B, locationSet)
-betweenness_centrality = bipartite.betweenness_centrality(B, locationSet)
+
+closenessCentrality = bipartite.closeness_centrality(B, locationSet)
+degreeCentrality = bipartite.degree_centrality(B, locationSet)
+betweennessCentrality = bipartite.betweenness_centrality(B, locationSet)
 #==============================================================================
 
 
@@ -62,14 +62,9 @@ i = 0
 for x in agentList:
     myActivities = othersActivities[othersActivities.cardID == x]
     othersActivities = othersActivities[othersActivities.cardID != x]
-    
+
 #    for activity in myActivities.itertuples():
-    
+
     print(len(othersActivities))
 
-   
 #==============================================================================
-
-    
-    
-    
