3687f1a475997e0bbd6bf361674af48153420f52
SIRAWIT BUTMARATTHAYA <sirawit.jome@mail.kmutt.ac.th>
2017-06-16 17:22:01 -0700

    final of this day ggwp

--- try.py
+++ try.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import networkx as nx
-from networkx.algorithms import bipartite
+#from networkx.algorithms import bipartite
 import matplotlib.pyplot as plt
 
 # searching on list of tuples
@@ -13,7 +13,7 @@ def to_seconds(line):
 
 
 print("reading file...") # 2015-12-25, ezLarge
-df = pd.read_csv("D:/KMUTT/_IHPC_Internship/Data/EZLink/ezLarge.csv")
+df = pd.read_csv("C:/_IHPC_Internship/Data/EZLink/ezLarge.csv")
 df = df[(df['travelMode'] == "Bus") & (df['endDate'] != '0000-00-00')]
 df = df[['cardID','busRegNum','busTripNum','startTime','endTime']]
 df['busTripNum'] = df['busTripNum'].map(int)
@@ -54,29 +54,32 @@ df.sort_values(['cardID', 'startTime'], ascending=[True, True], inplace=True)
 #==============================================================================
 # Agent Social Network
 #==============================================================================
-G = nx.MultiGraph()
+G = nx.Graph(name="Agent Social Network")
 #G.add_nodes_from(agentList)
 
 othersActivities = df
 
-
+#i = 0
 for myAgent in agentList:
+#    if i == 100:
+#        break
     myActivities = othersActivities[othersActivities['cardID'] == myAgent]
     othersActivities = othersActivities[othersActivities['cardID'] != myAgent]
-
+    
     G.add_node(myAgent)
-
     for activity in myActivities.itertuples():
         nearbyAgent = othersActivities[
             (othersActivities['busRegNum'] == activity[2]) \
             & (othersActivities['busTripNum'] == activity[3]) \
             & ((activity[4] <= othersActivities['endTime']) & (othersActivities['startTime'] <= activity[5])) \
-        ]       
+        ]
+        
         for agent in nearbyAgent.itertuples():
             overlap = min(activity[5], agent[5]) - max(activity[4], agent[4])
             G.add_node(agent[1])
             G.add_edge(activity[1],agent[1], weight = overlap)
-
+#    i = i + 1
+   
 #==============================================================================
 
 #==============================================================================
@@ -89,11 +92,11 @@ esmall=[(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] <=300]
 pos=nx.spring_layout(G) # positions for all nodes
 
 # nodes
-nx.draw_networkx_nodes(G,pos,node_size=20)
+nx.draw_networkx_nodes(G,pos,node_size=1)
 
 # edges
-nx.draw_networkx_edges(G,pos,edgelist=elarge,width=3)
-nx.draw_networkx_edges(G,pos,edgelist=esmall,width=3,alpha=0.3,edge_color='b',style='dashed')
+nx.draw_networkx_edges(G,pos,edgelist=elarge,width=1)
+nx.draw_networkx_edges(G,pos,edgelist=esmall,width=1,alpha=0.5,edge_color='b')
 
 # labels
 # nx.draw_networkx_labels(G,pos,font_size=5,font_family='sans-serif')
@@ -102,3 +105,16 @@ plt.axis('off')
 plt.savefig("weighted_graph.png") # save as png
 plt.show() # display
 #==============================================================================
+
+degree_all = G.degree()
+degree_centrality = nx.degree_centrality(G)
+
+degree_count = 0
+zero_count = 0
+for key, value in degree_centrality.items():
+    if value == 0:
+        zero_count = zero_count + 1
+    degree_count = degree_count + value
+    
+max_centrality = max(degree_centrality, key=lambda key: degree_centrality[key])
+        
