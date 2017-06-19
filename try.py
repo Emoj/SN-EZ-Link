dda19a50ed9e9226f8cd5ae19ef9acab7f1eda07
SIRAWIT BUTMARATTHAYA <sirawit.jome@mail.kmutt.ac.th>
2017-06-19 16:17:55 -0700

    SN with some analytics

--- try.py
+++ try.py
@@ -1,17 +1,26 @@
 import pandas as pd
 import networkx as nx
-#from networkx.algorithms import bipartite
 import matplotlib.pyplot as plt
+#from networkx.algorithms import bipartite
 
 # searching on list of tuples
 # [i for i, v in enumerate(locationList) if v[0] == 1 and v[1] == 1]
 
+# loop thru graph         
+#[ (u,v,edata['weight']) for u,v,edata in G.edges(data=True) if 'weight' in edata ]
 
+
+#==============================================================================
+# Convert time string to integer as number of seconds
+#==============================================================================
 def to_seconds(line):
     hh, mm, ss = [int(x) for x in line.split(':')]
     return hh*3600 + mm*60 + ss
 
 
+#==============================================================================
+# Reading input file and manipulate dataset
+#==============================================================================
 print("reading file...") # 2015-12-25, ezLarge
 df = pd.read_csv("C:/_IHPC_Internship/Data/EZLink/ezLarge.csv")
 df = df[(df['travelMode'] == "Bus") & (df['endDate'] != '0000-00-00')]
@@ -32,7 +41,7 @@ df.sort_values(['cardID', 'startTime'], ascending=[True, True], inplace=True)
 
 
 #==============================================================================
-# Try bipartite
+# Try bipartite ; need agent list and location list
 #==============================================================================
 # B = nx.MultiGraph()
 # B.add_nodes_from(agentList, bipartite=0)
@@ -48,73 +57,92 @@ df.sort_values(['cardID', 'startTime'], ascending=[True, True], inplace=True)
 # closenessCentrality = bipartite.closeness_centrality(B, locationSet)
 # degreeCentrality = bipartite.degree_centrality(B, locationSet)
 # betweennessCentrality = bipartite.betweenness_centrality(B, locationSet)
-#==============================================================================
 
 
 #==============================================================================
 # Agent Social Network
 #==============================================================================
-G = nx.Graph(name="Agent Social Network")
-#G.add_nodes_from(agentList)
+G = nx.MultiDiGraph(name="Agent Social Network")
+G.add_nodes_from(agentList)
 
 othersActivities = df
 
-#i = 0
+i = 0
 for myAgent in agentList:
-#    if i == 100:
+#    if i == 20:
 #        break
     myActivities = othersActivities[othersActivities['cardID'] == myAgent]
     othersActivities = othersActivities[othersActivities['cardID'] != myAgent]
     
-    G.add_node(myAgent)
     for activity in myActivities.itertuples():
         nearbyAgent = othersActivities[
             (othersActivities['busRegNum'] == activity[2]) \
             & (othersActivities['busTripNum'] == activity[3]) \
             & ((activity[4] <= othersActivities['endTime']) & (othersActivities['startTime'] <= activity[5])) \
         ]
-        
         for agent in nearbyAgent.itertuples():
             overlap = min(activity[5], agent[5]) - max(activity[4], agent[4])
-            G.add_node(agent[1])
             G.add_edge(activity[1],agent[1], weight = overlap)
-#    i = i + 1
-   
-#==============================================================================
+    i = i + 1
+#    print("Agent#" + str(i) + " out of " + str(len(agentList)))
+
 
 #==============================================================================
-# Draw a graph
+# Graph Visualization
 #==============================================================================
-
 elarge=[(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] >300]
 esmall=[(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] <=300]
 
-pos=nx.spring_layout(G) # positions for all nodes
+pos = nx.spring_layout(G) # positions for all nodes
 
 # nodes
-nx.draw_networkx_nodes(G,pos,node_size=1)
+nx.draw_networkx_nodes(G,pos,node_size=5)
 
 # edges
 nx.draw_networkx_edges(G,pos,edgelist=elarge,width=1)
 nx.draw_networkx_edges(G,pos,edgelist=esmall,width=1,alpha=0.5,edge_color='b')
 
-# labels
-# nx.draw_networkx_labels(G,pos,font_size=5,font_family='sans-serif')
-
 plt.axis('off')
-plt.savefig("weighted_graph.png") # save as png
+plt.savefig("weighted_graph.svg") # save as png
 plt.show() # display
-#==============================================================================
 
+
+#==============================================================================
+# Some analytics
+#==============================================================================
 degree_all = G.degree()
 degree_centrality = nx.degree_centrality(G)
 
 degree_count = 0
 zero_count = 0
-for key, value in degree_centrality.items():
+
+for key, value in degree_all.items():
     if value == 0:
         zero_count = zero_count + 1
-    degree_count = degree_count + value
+
+print("node with zero degree: " + str(zero_count))
     
 max_centrality = max(degree_centrality, key=lambda key: degree_centrality[key])
+max_degree = max(degree_all, key=lambda key: degree_centrality[key])
+
+for key, value in degree_centrality.items():
+    if value == degree_centrality[max_centrality]:
+        print(key + ": " + str(value))
         
+
+#==============================================================================
+# Check for no neighbor  #ezLarge:82
+#==============================================================================
+i = 0
+for myAgent in agentList:
+    myActivities = df[df['cardID'] == myAgent]
+    othersActivities = df[df['cardID'] != myAgent]
+    
+    for activity in myActivities.itertuples():
+        nearbyAgent = othersActivities[
+            (othersActivities['busRegNum'] == activity[2]) \
+            & (othersActivities['busTripNum'] == activity[3]) \
+            & ((activity[4] <= othersActivities['endTime']) & (othersActivities['startTime'] <= activity[5])) \
+        ]
+        if nearbyAgent.empty:
+            i = i + 1
